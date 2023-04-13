51a52331861564708625d86babaa9b64199b1260
ranjeet.singh1 <ranjeet.singh@circles.asia>
2023-04-13 17:47:32 +0530

    initial commit

---
+++ app/src/main/java/com/example/movieapp/ui/MovieDetail.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class MovieDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
